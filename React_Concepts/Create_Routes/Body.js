ebcf38a90f5fce5f3cdea9843524e1647c78851b
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-10 19:15:11 +0530

    Update Body.js

--- React_Concepts/Create_Routes/Body.js
+++ React_Concepts/Create_Routes/Body.js
@@ -6,6 +6,7 @@ import {
 import Home from "./Home";
 import Product from "./Product";
 import ProductDetails from "./ProductDetails";
+import Error from "./Error";
 
 const Body = () => {
   {
@@ -13,17 +14,26 @@ const Body = () => {
   }
   const appRouters = createBrowserRouter([
     {
-      path: "/",
-      element: (
-        <Navigate to="/Home" replace />
-      ) /*** To redirect Navigation to home when the path is fully empty */,
+      errorElement: <Error />, //Use errorElement as the key and value will be the Error element
+      children: [
+        //Mistake you have to place the entire objects insdie an array names childres
+        {
+          path: "/",
+          element: (
+            <Navigate to="/home" replace />
+          ) /*** To redirect Navigation to home when the path is fully empty */,
+        },
+        {
+          path: "/home",
+          element: <Home />,
+        },
+        { path: "/products", element: <Product /> },
+        {
+          path: "/ProductDetail/:id",
+          element: <ProductDetails />,
+        },
+      ],
     },
-    {
-      path: "/home",
-      element: <Home />,
-    },
-    { path: "/products", element: <Product /> },
-    { path: "/ProductDetail/:id", element: <ProductDetails /> },
   ]);
 
   return <RouterProvider router={appRouters}></RouterProvider>;
