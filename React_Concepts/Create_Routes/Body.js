95a0fbbdb43104d2d9e70d13fd69e56c8169dbca
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-10 17:45:55 +0530

    Update Body.js

--- React_Concepts/Create_Routes/Body.js
+++ React_Concepts/Create_Routes/Body.js
@@ -1,8 +1,14 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom"; /***** MISTAKE react router dom has to be installed */
 import Home from "./Home";
 import Product from "./Product";
 
 const Body = () => {
+  {
+    /***** MISTAKE CreateBrowserRoute has to be imported from react-router-dom*/
+  }
   const appRouters = createBrowserRouter([
     {
       path: "/home",
@@ -12,6 +18,9 @@ const Body = () => {
   ]);
 
   return <RouterProvider router={appRouters}></RouterProvider>;
+  {
+    /**** MISTAKE RouterProvider has to be  returned and it will accept a gear caled router*/
+  }
 };
 
 export default Body;
