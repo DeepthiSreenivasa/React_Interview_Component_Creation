08e20d89aa529e23d914374d961ee385d1c34a63
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-10 17:46:13 +0530

    Update Home.js

--- React_Concepts/Create_Routes/Home.js
+++ React_Concepts/Create_Routes/Home.js
@@ -1,5 +1,13 @@
+import { Link } from "react-router-dom"; /////// MISTAKE Link also has to be exported
+
 const Home = () => {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <h1>This Home Page</h1>
+      <Link to="/products">Product Page</Link>{" "}
+      {/****  MISTAKE Link tag has to be used along with to */}
+    </>
+  );
 };
 
 export default Home;
