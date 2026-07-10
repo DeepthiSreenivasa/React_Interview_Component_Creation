e18edccd540f42ce10109ed631d75c3f16d15677
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-10 19:36:08 +0530

    Update Body.js

--- React_Concepts/Create_Routes/Body.js
+++ React_Concepts/Create_Routes/Body.js
@@ -7,6 +7,7 @@ import Home from "./Home";
 import Product from "./Product";
 import ProductDetails from "./ProductDetails";
 import Error from "./Error";
+import App from "./App";
 
 const Body = () => {
   {
@@ -14,22 +15,25 @@ const Body = () => {
   }
   const appRouters = createBrowserRouter([
     {
-      errorElement: <Error />, //Use errorElement as the key and value will be the Error element
+      path: "/",
+      element: <App />, //Mistake Added element App here as parent to fix router-outlet
+      //Mistakke Use errorElement as the key and value will be the Error element
+      errorElement: <Error />,
       children: [
         //Mistake you have to place the entire objects insdie an array names childres
         {
-          path: "/",
+          index: true,
           element: (
-            <Navigate to="/home" replace />
+            <Navigate to="home" replace />
           ) /*** To redirect Navigation to home when the path is fully empty */,
         },
         {
-          path: "/home",
+          path: "home",
           element: <Home />,
         },
-        { path: "/products", element: <Product /> },
+        { path: "products", element: <Product /> },
         {
-          path: "/ProductDetail/:id",
+          path: "ProductDetail/:id",
           element: <ProductDetails />,
         },
       ],
