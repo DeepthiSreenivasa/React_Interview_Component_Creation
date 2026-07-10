03e3de99afe3ae596a9401255ff26fd3265344c5
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-10 18:12:25 +0530

    Update Product.js

--- React_Concepts/Create_Routes/Product.js
+++ React_Concepts/Create_Routes/Product.js
@@ -1,5 +1,25 @@
+import { Link } from "react-router-dom";
+import { ProductDetails } from "./ProductDetails";
+
 const Product = () => {
-  return <h1>This Product Page</h1>;
+  const products = [
+    { productName: "Phone", id: 1 },
+    { productName: "Book", id: 2 },
+    { productName: "Pen", id: 3 },
+  ];
+
+  return (
+    <>
+      <h1>This Product Page</h1>
+
+      {products.map((item) => (
+        <div>
+          <Link to={`/productDetail/${item.id}`}>{item.productName}</Link>
+          {/**MISTAKE - Without leading '/' the page might route considering "product/productDetail" */}
+        </div>
+      ))}
+    </>
+  );
 };
 
 export default Product;
