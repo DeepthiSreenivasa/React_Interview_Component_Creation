7714f73cd4aebf2586b9d0e667b454f6f5dac87f
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-12 15:22:31 +0530

    Create ProductDetail.js

---
+++ React_Concepts/With_Search_Functionality/ProductDetail.js
@@ -0,0 +1,39 @@
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+
+const ProductDetail = () => {
+  const param = useParams();
+  const [productDetail, setProductDetail] = useState({}); //Mistake initial State was set to {}
+  const productURL = "https://dummyjson.com/products";
+
+  useEffect(() => {
+    getProductData();
+  }, []);
+
+  const getProductData = async () => {
+    let url = productURL + "/" + param.id;
+    let data = await fetch(url);
+    let dataFromApi = await data.json();
+    console.log("dataFromApi::", dataFromApi);
+    setProductDetail(dataFromApi);
+  };
+
+  return productDetail ? (
+    <>
+      <h1>ProductDetailPage</h1>
+      <div>{productDetail?.id}</div>
+      <div>{productDetail?.title}</div>
+      <div>{productDetail?.description}</div>
+      <div>
+        <img src={productDetail?.images?.[0]} width="200px" height="200px" />{" "}
+        {/**Mistakkke missed to give '?' after images */}
+      </div>
+    </>
+  ) : (
+    <>
+      <h1>No Products Found</h1> {/***Mistake h1 was not closed properly */}
+    </>
+  );
+};
+
+export default ProductDetail;
