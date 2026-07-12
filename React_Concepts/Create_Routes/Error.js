dd51de85dc20127e2e99fcf0558047e394d257d1
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-12 13:02:21 +0530

    Update Error.js

--- React_Concepts/Create_Routes/Error.js
+++ React_Concepts/Create_Routes/Error.js
@@ -1,7 +1,7 @@
 import { useRouteError } from "react-router-dom";
 
 const Error = () => {
-  const err = useRouteError(); ///Mistake you have to use this hook
+  const err = useRouteError(); ///Mistake you have to use this hook ////Misate you forgot the name it is useRouteError
 
   return <div>You have an error in the route</div>;
 };
