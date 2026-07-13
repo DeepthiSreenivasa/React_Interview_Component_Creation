774cba3b2096ad0dbb99fa39882e59a52b7ce076
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-14 00:40:23 +0530

    Create App.js

---
+++ React_Concepts/With_UserContext/App.js
@@ -0,0 +1,54 @@
+import "./styles.css";
+import Home from "./Home";
+import Product from "./Product";
+import ProductDetail from "./ProductDetail";
+import Login from "./Login";
+import {
+  RouterProvider,
+  createBrowserRouter,
+  Navigate,
+} from "react-router-dom";
+import { useState } from "react";
+import UserDetailsContext from "./UserDetailsContext";
+//import Error from "./Error";
+
+export default function App() {
+  const [userFirstName, setUserFirstName] = useState("Deepthi");
+  const appRoutes = createBrowserRouter([
+    {
+      index: true, //Mistake : you forgot what happens when empty route is given
+      // You have to add index, create Navigate element and also you
+      //have to create another path and element pair like below
+      element: <Navigate to="Home" replace />,
+    },
+    {
+      path: "Login",
+      element: <Login></Login>,
+    },
+    {
+      path: "Home",
+      //Mistake you should use capital letter for Provider
+
+      element: <Home></Home>,
+    },
+    {
+      path: "Product",
+      element: <Product></Product>,
+    },
+    {
+      path: "ProductDetail/:id",
+      element: <ProductDetail></ProductDetail>,
+    },
+  ]);
+
+  return (
+    <UserDetailsContext.Provider //Mistake this should be capital P in provider
+      value={{
+        userFirstName: userFirstName,
+        setUserFirstName: (name) => setUserFirstName(name), //Mistake you have to call the setter function again from Login page
+      }}
+    >
+      <RouterProvider router={appRoutes}></RouterProvider>
+    </UserDetailsContext.Provider>
+  );
+}
