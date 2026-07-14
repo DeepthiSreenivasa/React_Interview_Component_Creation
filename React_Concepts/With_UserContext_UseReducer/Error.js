a8ecf7c138ee4cdb5832ead77d42f64d9e3aaaf2
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-14 11:00:45 +0530

    Delete React_Concepts/With_UserContext_UseReducer/Error.js

--- React_Concepts/With_UserContext_UseReducer/Error.js
+++
@@ -1,9 +0,0 @@
-import { useRouteError } from "react-router-dom";
-
-const Error = () => {
-  const err = useRouteError(); //Misate you forgot the name it is useRouteError
-
-  return <h1>You landed on an invalid Route</h1>;
-};
-
-export default Error;
