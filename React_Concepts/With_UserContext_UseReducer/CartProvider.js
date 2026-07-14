edd54a2d61608e447f444d3e3bb7289791d8f627
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-14 11:02:15 +0530

    Create CartProvider.js

---
+++ React_Concepts/With_UserContext_UseReducer/CartProvider.js
@@ -0,0 +1,20 @@
+import { useReducer } from "react";
+import CartQReducer from "./CartQReducer";
+import CartQContext from "./CartQContext";
+
+const initialState = {
+  quantity: 0,
+};
+
+console.log("CartReducer =", CartQReducer);
+const CartProvider = ({ children }) => {
+  console.log("CartProvider Rendered");
+  const [state, dispatch] = useReducer(CartQReducer, initialState);
+
+  return (
+    <CartQContext.Provider value={{ quantity: state.quantity, dispatch }}>
+      {children}
+    </CartQContext.Provider>
+  );
+};
+export default CartProvider;
