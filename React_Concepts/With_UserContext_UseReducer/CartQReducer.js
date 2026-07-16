2c638296b7e63e66e034f5063105cfa53d52e0a9
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-16 22:50:55 +0530

    Update CartQReducer.js

--- React_Concepts/With_UserContext_UseReducer/CartQReducer.js
+++ React_Concepts/With_UserContext_UseReducer/CartQReducer.js
@@ -1,6 +1,6 @@
-const CartQReducer = (state, action) => { //reducer file is jus a normal function with switch case
+const CartQReducer = (state, action) => { //MISTAKE ::reducer file is jus a normal function with switch case
   console.log("Reducer called", action);
-  switch (action.type) {
+  switch (action.type) { //MISTAKE : dont forget the key name for action
     case "ADD_ITEMS": {
       console.log("Into ADD_ITEM");
       return { ...state, quantity: state.quantity + 1 };
