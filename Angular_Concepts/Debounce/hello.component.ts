b64d771223d6bb1089f54958f8c9939c82fcd22e
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-27 11:29:51 +0530

    Add HelloComponent with search and debounce
    Implement search functionality with debounce in HelloComponent.

---
+++ Angular_Concepts/Debounce/hello.component.ts
@@ -0,0 +1,45 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import {
+  debounce,
+  debounceTime,
+  distinctUntilChanged,
+  Subject,
+  switchMap,
+} from 'rxjs';
+
+@Component({
+  selector: 'app-hello',
+  templateUrl: './hello.component.html',
+  styleUrls: ['./hello.component.css'],
+  standalone: true,
+  imports: [HttpClientModule, CommonModule, FormsModule],
+})
+export class HelloComponent implements OnInit {
+  public http: HttpClient = inject(HttpClient);
+  public products: any[] = [];
+  public searchText$ = new Subject<String>();
+  public searchText: String = '';
+
+  ngOnInit() {
+    this.searchText$
+      .pipe(
+        debounceTime(100),
+        distinctUntilChanged(),
+        switchMap((searchText) => {
+          return this.http.get<any>(
+            'https://dummyjson.com/products/search?q=' + searchText
+          );
+        })
+      )
+      .subscribe((data) => {
+        console.log('Search Results::', data);
+      });
+  }
+
+  public getSearchText(event: any) {
+    this.searchText$.next(event.target.value);
+  }
+}
