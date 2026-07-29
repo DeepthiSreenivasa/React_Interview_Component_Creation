adc813e63daf1bd4e9d233c34ea23d8c44b8b953
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-29 17:31:32 +0530

    Update ANY.js

--- JS_Concepts/ProimiseAPI/ANY.js
+++ JS_Concepts/ProimiseAPI/ANY.js
@@ -1,1 +1,25 @@
 
+let productId = [ '8k', 18];
+
+function getProductDetails() {
+
+    let promiseObj = productId.map((item) => fetch("https://dummyjson.com/products/" + item).then((res) => { if (!res.ok) { throw new Error('Product not found!') }; return res.json() }))
+
+
+        Promise.any(promiseObj).then(data => console.log(data)).catch((err) => console.log("Err:::", err))
+
+}
+
+getProductDetails()
+
+//NOTE you can never cancel a promise in between
+
+
+/***********ANY*************/
+
+//Will give back an obj nor an array
+
+//SUCCESS CASE : 
+//FAILURE CASE :
+
+//For both the above cases it will return who FULLFILLED only (SUCCESS PROMISE ONLY)
