7085be932f4c093a06aa3dac287403e17e45b466
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-29 13:08:29 +0530

    Update CartQContext.js

--- React_Concepts/With_UserContext_UseReducer/CartQContext.js
+++ React_Concepts/With_UserContext_UseReducer/CartQContext.js
@@ -1,5 +1,5 @@
 import { createContext } from "react";
 
-const CartQContext = createContext();
+const CartQContext = createContext(); // this is just empty dont need to addanything here
 
 export default CartQContext;
