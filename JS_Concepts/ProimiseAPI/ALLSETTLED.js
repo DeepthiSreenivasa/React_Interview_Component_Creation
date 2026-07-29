da047f8e7a559307e7956f79f9e3d9050a56e05b
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-29 17:21:03 +0530

    Update ALLSETTLED.js

--- JS_Concepts/ProimiseAPI/ALLSETTLED.js
+++ JS_Concepts/ProimiseAPI/ALLSETTLED.js
@@ -1,1 +1,23 @@
 
+let productId = [1, '2j', 3];
+
+function getProductDetails() {
+
+    let promiseArr = productId.map((item) => fetch("https://dummyjson.com/products/" + item).then((res) => { if (!res.ok) { throw new Error('Product not found!') }; return res.json() }))
+
+
+    if (promiseArr.length > 0) {
+        Promise.allSettled(promiseArr).then(data => console.log(data)).catch((err) => console.log("Err:::", err))
+    }
+
+}
+
+getProductDetails()
+
+//NOTE you can never cancel a promise in between
+
+
+/***********ALL SETTLED*************/
+
+//SUCCESS CASE : All start simultaneously and will wait for all of them to complete and give back the result
+//FAILURE CASE : The failed promise will not be setlled the other promises will be fulfilled
