2e7fb74aaaf4166fecdf14b59c5a871824a424c4
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-07-29 17:30:16 +0530

    Add getProductDetails function using Promise.race
    Implement Promise.race to fetch product details concurrently.

--- JS_Concepts/ProimiseAPI/RACE.js
+++ JS_Concepts/ProimiseAPI/RACE.js
@@ -1,1 +1,24 @@
+let productId = [ '8k', 18];
 
+function getProductDetails() {
+
+    let promiseObj = productId.map((item) => fetch("https://dummyjson.com/products/" + item).then((res) => { if (!res.ok) { throw new Error('Product not found!') }; return res.json() }))
+
+
+        Promise.race(promiseObj).then(data => console.log(data)).catch((err) => console.log("Err:::", err))
+
+}
+
+getProductDetails()
+
+//NOTE you can never cancel a promise in between
+
+
+/***********RACE*************/
+
+//Will give back an obj or an array
+
+//SUCCESS CASE : 
+//FAILURE CASE :
+
+//For both the above cases it will return who fullfilled or rejected  first
