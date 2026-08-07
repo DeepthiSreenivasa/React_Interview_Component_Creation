f37a4d78e1c1835301db7ed30bd16bc180f86d7d
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-08-07 13:34:46 +0530

    Add StarRating component for user ratings

---
+++ Components/Star Rating/StarRating.jsx
@@ -0,0 +1,58 @@
+import { useState } from "react";
+
+const StarRating = () => {
+  let stars = [0, 1, 2, 3, 4];
+
+  const [filledStars, setFilledStars] = useState([]);
+  const [ratedStars, setRatedStars] = useState([]);
+
+  const fillStars = (index) => {
+    let temp = [];
+
+    for (var i = 0; i <= index; i++) {
+      temp.push(i);
+    }
+
+    setFilledStars(temp);
+  };
+
+  const unFillStars = () => {
+    setFilledStars([]);
+  };
+
+  const setRating = (index) => {
+    let temp = [];
+
+    for (var i = 0; i <= index; i++) {
+      temp.push(i);
+    }
+
+    setFilledStars(temp);
+    setRatedStars(temp);
+  };
+
+  return (
+    <>
+      <h1>Star Component</h1>
+      {stars.map((item, index) => (
+        <span
+          onMouseEnter={() => {
+            fillStars(index);
+          }}
+          onMouseLeave={() => {
+            unFillStars();
+          }}
+          onClick={() => {
+            setRating(index);
+          }}
+        >
+          {filledStars.includes(index) || ratedStars.includes(index)
+            ? "★"
+            : "☆"}
+        </span>
+      ))}
+    </>
+  );
+};
+
+export default StarRating;
