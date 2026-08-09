dfc0130c5426a06694d8d79524b3be6ae4f06e1c
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-08-09 12:08:59 +0530

    Delete Components/Like.jsx

--- Components/Like.jsx
+++
@@ -1,9 +0,0 @@
-const Like = ({ value = false, onChange }) => {
-  const updateLikeStatus = () => {
-    onChange(!value);
-  };
-
-  return <div onClick={updateLikeStatus}>{value ? "♥" : "♡"}</div>;
-};
-
-export default Like;
