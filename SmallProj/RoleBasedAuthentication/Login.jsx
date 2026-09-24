ec3ced3f720947127a1659b083907e3669c830bf
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-09-24 21:20:26 +0530

    Create Login.jsx

---
+++ SmallProj/RoleBasedAuthentication/Login.jsx
@@ -0,0 +1,59 @@
+import { useRef, useEffect, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import UserContext from "./userContext";
+
+const Login = () => {
+  const username = useRef("");
+  const password = useRef("");
+  const navigate = useNavigate();
+
+  const { userDetails, setUserDetails } = useContext(UserContext);
+
+  function loginUser() {
+    console.log("UserName::", username.current);
+
+    fetch("https://dummyjson.com/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        username: username.current,
+        password: password.current,
+      }),
+    })
+      .then((res) => {
+        if (!res.ok) throw new Error("Something went wrong");
+        return res.json();
+      })
+      .then((data) => {
+        setUserDetails({
+          name: data.firstName,
+          age: data.age,
+          email: data.email,
+          role: "admin",
+        });
+        console.log("userDetails::", userDetails);
+        navigate("/Dashboard");
+      })
+      .catch((err) => console.log("Error::", err));
+  }
+
+  return (
+    <>
+      <label>User Name</label>
+      <input
+        type="text"
+        onChange={(event) => (username.current = event.target.value)}
+      />
+      <label>Password</label>
+      <input
+        type="text"
+        onChange={(event) => (password.current = event.target.value)}
+      />
+      <button onClick={loginUser}>Submit</button>
+    </>
+  );
+};
+
+export default Login;
