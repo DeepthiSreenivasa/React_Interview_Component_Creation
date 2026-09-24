6fdca1cc5bdb2b9b32dd1c0b76cbfe7a8342bd42
Deepthi S <121350337+DeepthiSreenivasa@users.noreply.github.com>
2026-09-24 21:22:00 +0530

    Create Users.jsx

---
+++ SmallProj/RoleBasedAuthentication/Users.jsx
@@ -0,0 +1,50 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+const Users = () => {
+  const [users, setUsers] = useState([]);
+  const [searchString, setSearchString] = useState("");
+
+  let filteredUsers = users.filter((item) =>
+    item.firstName.includes(searchString)
+  );
+
+  useEffect(() => {
+    let controller = new AbortController();
+    getUserDetails();
+  }, []);
+
+  function getFilteredUsers(event) {
+    let searchStr = event.target.value;
+    setSearchString(searchStr);
+  }
+
+  function getUserDetails() {
+    fetch("https://dummyjson.com/users")
+      .then((res) => res.json())
+      .then((data) => {
+        userData = data.users;
+        filteredUsers = [...userData];
+        console.log("filteredUsers::", filteredUsers);
+        setUsers(userData);
+      });
+  }
+
+  return (
+    <>
+      <Link to="/Admin">Go to Admin Page</Link>
+      <input
+        type="text"
+        value={searchString}
+        onChange={($event) => getFilteredUsers($event)}
+      />
+      <ul>
+        {filteredUsers.map((user) => (
+          <li>{user.firstName}</li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Users;
